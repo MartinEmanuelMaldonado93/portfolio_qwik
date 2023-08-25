398bc61e616855e30105bbf68ad4b7503a5508a9
Martin Maldonado <martinemanuel93@gmail.com>
2023-08-25 17:33:14 -0300

    comment react integration

--- src/integrations/react/framerGallery.tsx
+++ src/integrations/react/framerGallery.tsx
@@ -1,6 +1,6 @@
 /** @jsxImportSource react */
-import { useState } from "react";
-import { motion, AnimatePresence } from "framer-motion";
+// import { useState } from "react";
+// import { motion, AnimatePresence } from "framer-motion";
 import { wrap } from "popmotion";
 import { qwikify$ } from "@builder.io/qwik-react";
 
@@ -13,89 +13,89 @@ const images = [
 
 const SwipeConfidenceThreshold = 10_000;
 
-export const ImageGallery = qwikify$(() => {
-	const [[page, direction], setPage] = useState([0, 0]);
+// export const ImageGallery = qwikify$(() => {
+// 	// const [[page, direction], setPage] = useState([0, 0]);
 
-	const imageIndex = wrap(0, images.length, page);
+// 	const imageIndex = wrap(0, images.length, page);
 
-	const paginate = (newDirection: number) => {
-		setPage([page + newDirection, newDirection]);
-	};
-	const variants = {
-		enter: (direction: number) => ({
-			x: direction > 0 ? 1000 : -1000,
-			opacity: 0,
-		}),
-		exit: (direction: number) => ({
-			zIndex: 0,
-			x: direction < 0 ? 1000 : -1000,// 
-			opacity: 0,
-		}),
-		center: {
-      x: 0,
-			opacity: 1,
-			zIndex: 1,
-		},
-	};
-	return (
-		<div className='grid grid-rows-1 grid-cols-1 max-w-md m-auto'>
-			<AnimatePresence initial={false} custom={direction}>
-				<motion.img
-					className='m-auto rounded-md'
-					style={{ gridArea: "1 / 1" }}
-					key={page}
-					src={images[imageIndex]}
-					custom={direction}
-					variants={variants}
-					initial='enter'
-					animate='center'
-					exit='exit'
-					drag='x'
-					dragConstraints={{ left: 0, right: 0 }}
-					dragElastic={1}
-					transition={{
-						x: { type: "spring", stiffness: 150, damping: 30 },
-						opacity: { duration: 0.2 },
-					}}
-					onDragEnd={(e, { offset, velocity }) => {
-						const swipe = swipePower(offset.x, velocity.x);
+// 	const paginate = (newDirection: number) => {
+// 		setPage([page + newDirection, newDirection]);
+// 	};
+// 	const variants = {
+// 		enter: (direction: number) => ({
+// 			x: direction > 0 ? 1000 : -1000,
+// 			opacity: 0,
+// 		}),
+// 		exit: (direction: number) => ({
+// 			zIndex: 0,
+// 			x: direction < 0 ? 1000 : -1000,// 
+// 			opacity: 0,
+// 		}),
+// 		center: {
+//       x: 0,
+// 			opacity: 1,
+// 			zIndex: 1,
+// 		},
+// 	};
+// 	return (
+// 		<div className='grid grid-rows-1 grid-cols-1 max-w-md m-auto'>
+// 			<AnimatePresence initial={false} custom={direction}>
+// 				<motion.img
+// 					className='m-auto rounded-md'
+// 					style={{ gridArea: "1 / 1" }}
+// 					key={page}
+// 					src={images[imageIndex]}
+// 					custom={direction}
+// 					variants={variants}
+// 					initial='enter'
+// 					animate='center'
+// 					exit='exit'
+// 					drag='x'
+// 					dragConstraints={{ left: 0, right: 0 }}
+// 					dragElastic={1}
+// 					transition={{
+// 						x: { type: "spring", stiffness: 150, damping: 30 },
+// 						opacity: { duration: 0.2 },
+// 					}}
+// 					onDragEnd={(e, { offset, velocity }) => {
+// 						const swipe = swipePower(offset.x, velocity.x);
 
-						if (swipe < -SwipeConfidenceThreshold) {
-							paginate(1);
-						} else if (swipe > SwipeConfidenceThreshold) {
-							paginate(-1);
-						}
-					}}
-				/>
-			</AnimatePresence>
-			<div className='flex justify-between px-10 w-full relative -translate-y-12 z-[3]'>
-				<Arrow paginate={paginate} dir={dir.prev} />
-				<Arrow paginate={paginate} dir={dir.next} />
-			</div>
-		</div>
-	);
-});
+// 						if (swipe < -SwipeConfidenceThreshold) {
+// 							paginate(1);
+// 						} else if (swipe > SwipeConfidenceThreshold) {
+// 							paginate(-1);
+// 						}
+// 					}}
+// 				/>
+// 			</AnimatePresence>
+// 			<div className='flex justify-between px-10 w-full relative -translate-y-12 z-[3]'>
+// 				<Arrow paginate={paginate} dir={dir.prev} />
+// 				<Arrow paginate={paginate} dir={dir.next} />
+// 			</div>
+// 		</div>
+// 	);
+// });
 
 enum dir {
 	next = 1,
 	prev = -1,
 }
-function Arrow({ paginate, dir }: { paginate: (n: number) => void; dir: dir }) {
-	return (
-		<div
-			className={
-				"flex justify-center items-center text-center text-black opacity-50 bg-gray-200 w-6 h-6 pointer font-bold text-xl z-[2] align-middle cursor-pointer hover:bg-gray-400 active:bg-gray-200 transition-all scale-75 hover:scale-100"
-			}
-			style={{
-				clipPath:
-					dir > 0
-						? "polygon(75% 0%, 100% 50%, 75% 100%, 0% 100%, 25% 50%, 0% 0%)"
-						: "polygon(100% 0%, 75% 50%, 100% 100%, 25% 100%, 0% 50%, 25% 0%)",
-			}}
-			onClick={() => paginate(dir)}
-		></div>
-	);
-}
+// function Arrow({ paginate, dir }: { paginate: (n: number) => void; dir: dir }) {
+// 	return (
+// 		<div
+// 			className={
+// 				"flex justify-center items-center text-center text-black opacity-50 bg-gray-200 w-6 h-6 pointer font-bold text-xl z-[2] align-middle cursor-pointer hover:bg-gray-400 active:bg-gray-200 transition-all scale-75 hover:scale-100"
+// 			}
+// 			style={{
+// 				clipPath:
+// 					dir > 0
+// 						? "polygon(75% 0%, 100% 50%, 75% 100%, 0% 100%, 25% 50%, 0% 0%)"
+// 						: "polygon(100% 0%, 75% 50%, 100% 100%, 25% 100%, 0% 50%, 25% 0%)",
+// 			}}
+// 			onClick={() => paginate(dir)}
+// 		></div>
+// 	);
+// }
 function swipePower(offset: number, velocity: number) {
 	return Math.abs(offset) * velocity;
 }
